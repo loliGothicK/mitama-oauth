1117e18fe895c6c622db2dc6174b8389b7ab0aed
mitama <loligothick@gmail.com>
2024-02-27 10:48:29 +0900

    chore: debug3

--- utils/parse-user.ts
+++ utils/parse-user.ts
@@ -5,18 +5,20 @@ import { config } from "./config";
 import { DiscordUser } from "./types";
 
 export function parseUser(ctx: GetServerSidePropsContext): DiscordUser | null {
+  console.log(ctx.req.headers.cookie);
   if (!ctx.req.headers.cookie) {
     return null;
   }
 
   const token = parse(ctx.req.headers.cookie)[config.cookieName];
-
+  console.log(token);
   if (!token) {
     return null;
   }
 
   try {
     const { iat, exp, ...user } = verify(token, config.jwtSecret) as DiscordUser & { iat: number; exp: number };
+    console.log(user);
     return user;
   } catch (e) {
     return null;
