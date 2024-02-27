11aa526e67cf43cc7ead464bc809e73fb6dba46d
mitama <loligothick@gmail.com>
2024-02-27 17:56:02 +0900

    feat: missingSuspenseWithCSRBailout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 'use client';
-import { config } from "@/utils/config";
-import { verify } from "jsonwebtoken";
 import {redirect, useSearchParams, useRouter} from "next/navigation";
-import { DiscordUser } from "@/utils/types";
-import {useEffect} from "react";
 
 export default function Index() {
   const router = useRouter();
