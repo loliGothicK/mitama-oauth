4f95b331f754a55c811f8f7aa50cb937b2c23e68
mitama <loligothick@gmail.com>
2024-03-04 19:23:12 +0900

    feat: beautiful logged in page?

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
 'use client';
 import {redirect, useSearchParams, useRouter} from "next/navigation";
+import {useEffect} from "react";
+import {Box, Typography} from "@mui/material";
 
 export default function Index() {
   const router = useRouter();
   const searchParams = useSearchParams();
+
   const token = searchParams.get('token');
   if (!token) {
     redirect(`/api/auth`);
   } else {
-    router.push(`mitamatch://auth?token=${token}`)
+    router.push(`mitamatch://auth?token=${token}`);
   }
-  return <h1>Successfully logged in.</h1>;
+  
+  useEffect(() => {
+    setTimeout(() => {
+      // Close the window after 10 seconds.
+      window.close();
+    }, 10 * 1000);
+  }, []);
+  
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Typography variant="h4">ログインに成功しました！</Typography>
+    </Box>
+  );
 }
